a26e6337412ab238a24c5ecbd7559d187623dd62
Narendrakumarkolli <30337207+narendrajava1@users.noreply.github.com>
2020-01-30 22:13:37 +0530

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,4 +1,8 @@
 def call (  def mvnHome,def goal,def pom){
-   sh '"${mvnHome}" -Dmaven.test.failure.ignore clean package'
+   rtMavenRun (
+      tool: "${mvnHome}",
+      pom: "${pom}",
+      goals: "${goal}"
+  )
   
 }
