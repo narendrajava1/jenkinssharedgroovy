fd27a705249ff648efb5dc02fef1f3e42231569b
Narendrakumarkolli <30337207+narendrajava1@users.noreply.github.com>
2020-01-30 21:49:11 +0530

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,9 +1,8 @@
-def call (  def mvnHome,def pom, def goal){
+def call (  def mvnHome,def goal,def pom){
    rtMavenRun (
       tool: "${mvnHome}",
       pom: "${pom}",
       goals: "${goal}",
-      opts: '-Xms1024m -Xmx4096m',
-      deployerId: 'deployer-unique-id',
+      opts: '-Xms1024m -Xmx4096m'
   )
 }
