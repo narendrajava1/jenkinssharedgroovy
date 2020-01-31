e5877deb84d1d7382afa4421b004847a8fa2b166
Narendrakumarkolli <30337207+narendrajava1@users.noreply.github.com>
2020-01-31 21:25:24 +0530

    Update dockerBuildAndPush.groovy

--- vars/dockerBuildAndPush.groovy
+++ vars/dockerBuildAndPush.groovy
@@ -1,6 +1,7 @@
 def call(def registry,def imageName){
 // This step should not normally be used in your script. Consult the inline help for details.
 withDockerRegistry(url: 'https://hub.docker.com/') {
+    sh "ls"
     sh label: '', script: "docker-compose up"
     
     }
