b6a7a33e374c1ab2b474ce00f478cdd93a699645
Narendrakumarkolli <30337207+narendrajava1@users.noreply.github.com>
2020-01-31 21:16:24 +0530

    Update dockerBuildAndPush.groovy

--- vars/dockerBuildAndPush.groovy
+++ vars/dockerBuildAndPush.groovy
@@ -1,7 +1,7 @@
 def call(def registry,def imageName){
 // This step should not normally be used in your script. Consult the inline help for details.
 withDockerRegistry(url: 'https://hub.docker.com/') {
-    sh label: '', script: "docker build -t ${registry}/${imageName}:${BUILD_NUMBER} ."
+    sh label: '', script: "docker build -t ${imageName}:${BUILD_NUMBER} ."
     
     }
   
